1dbbe3b7bc6323df475c721841179eed4d538893
Abhijith236 <125438175+Abhijith236@users.noreply.github.com>
2023-02-20 18:52:46 +0530

    Update user.go

--- models/user.go
+++ models/user.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-	Id       uint   //'json:"id"'
-	UserID   string //'json:"user_id gorm:"unique"'
-	Password []byte //json:"-"
+	Id       uint   `json:"id"`
+	UserID   string `json:"user_id" gorm:"unique"`
+	Password []byte `json:"-"`
 }
